4c1643be6971549393aa646208bafbe30c0c2ac4
Elvis Pfützenreuter <epxx@epxx.co>
2021-12-02 12:19:02 -0300

    Add comments

--- LoRa-trans.ino
+++ LoRa-trans.ino
@@ -20,6 +20,8 @@ void oled_show(const char* msg, const char *msg2 = 0);
 
 void setup()
 {
+	// show some sort of unique device ID in debug messages
+	// (not strictly necessary)
 	uint64_t chipid = ESP.getEfuseMac();
 	uint16_t hash = (uint16_t)(chipid >> 32);
 	snprintf(myid, 5, "%04X", hash);
@@ -27,6 +29,8 @@ void setup()
 	Serial.begin(115200);
 	Serial.print("Starting, ID is ");
 	Serial.println(myid);
+
+	// Pass NULL as encryption key for cleartext communication
 	l2 = new LoRaL2(BAND, SPREAD, BWIDTH,
 			ENCRYPTION_KEY, strlen(ENCRYPTION_KEY),
 			packet_received);
@@ -55,6 +59,7 @@ void send_packet()
 		return;
 	}
 
+	// generate an example payload
 	String payload = String(myid) + " " + String(arduino_millis() / 1000) + "s ";
 	int send_size = arduino_random(6, l2->max_payload() + 1);
 	while (payload.length() < send_size) {
@@ -63,7 +68,8 @@ void send_packet()
 
 	String msg = "Send ";
 	String msg2 = "  len " + String(payload.length());
-	
+
+	// send() does NOT take ownership of payload
 	if (!l2->send((const uint8_t*) payload.c_str(), payload.length())) {
 		msg += " (ERROR)";
 	} else {
@@ -78,6 +84,7 @@ void send_packet()
 			arduino_random(0, HALF_SEND_INTERVAL * 2);
 }
 
+// called by loop()
 void handle_received_packet()
 {
 	if (! pending_recv) {
@@ -102,14 +109,14 @@ void handle_received_packet()
 
 
 // RX callback
-// Takes the ownership of pkt and its internal buffer
-// Do as little as possible here (e.g. oled_show() may crash)
-
+// Takes the ownership of packet and its contents
+// Do as little as possible here! (E.g. oled_show() may crash)
 void packet_received(LoRaL2Packet *pkt)
 {
 	if (pending_recv) {
 		delete pending_recv;
 	}
+	// leave the packet for handle_received_packet() to handle
 	pending_recv = pkt;
 }
 
